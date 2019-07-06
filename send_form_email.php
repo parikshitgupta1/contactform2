e05704fd2e2a148e657e083f28167beed84392aa
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2019-07-06 23:22:48 +0530

    mailid

--- send_form_email.php
+++ send_form_email.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "parikshitvmgupta@gmail.com";
+    $email_to = "parikshit@gmail.com";
     $email_subject = "Form Submission";
  
     function died($error) {
